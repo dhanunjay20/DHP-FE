0bf0d96555a593ce1d1942527e57f21f682e67a9
SudaYasaswini <iamyasaswini02@gmail.com>
2025-09-25 17:03:40 +0530

    stats and our mission components created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import Header from './components/Header'
+import Stats from './components/Stats';
+import OurMission from './components/OurMission';
+import Header from './components/Header';
 import Footer from './components/Footer'
 const App = () => {
   return (
     <div>
-      <Header />
+      <Stats />
+      <OurMission />
     </div>
   )
 }
